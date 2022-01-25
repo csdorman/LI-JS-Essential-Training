3f7a49962247541ee65e693158f0ad407c43fd42
csdorman <csdorman@gmail.com>
2022-01-25 12:07:33 -0500

    Finished first challenge (creating objects)

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,51 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const laptop = {
+  name: "MacBook Pro 13' 2015",
+  ram: 8,
+  storage: 128,
+  screen: 13,
+  battery: 50,
+  lidOpen: false,
+  onBattery: false,
+  inUse: function (inUse) {
+    this.lidOpen = inUse
+    this.onBattery = inUse
+  }
+}
+
+const hornCase = {
+  brand: "Wiseman Detachable Bell Case",
+  containsHorn: true,
+  contents: {
+    horn: "Paxman 75M",
+    mutes: {
+      mute1: "Balu Straight",
+      mute2: "Balu Practice",
+    },
+    mouthpieces: {
+      mp1: "H1 cup",
+      mp2: "SF cup",
+      mp3: "H3 cup"
+    },
+    accessories: {
+      mouthpiecePouch: true,
+      pencil: true,
+      oilPouch: true,
+    }
+  },
+  lidOpen: true,
+  handles: 2,
+  strapLength: {
+    left: 20,
+    right: 20,
+  }
+}
+
+console.log(laptop)
+
+laptop.inUse(true)
+console.log(laptop)
+console.log(hornCase.contents)

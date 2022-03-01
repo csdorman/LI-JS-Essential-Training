61f1d08f8b835fe01a22fe483480224832368309
csdorman <csdorman@gmail.com>
2022-03-01 12:17:34 -0500

    Creating three functions

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,38 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+// Function Declaration
+function newParagraph() {
+  console.log("inside newParagraph")
+  const lastPara = document.querySelector(".para2")
+  const newText = document.createElement("p")
+  newText.innerText = "This is a new paragraph after the previous ones."
+  lastPara.append(newText)
+}
+
+// Call function declaration
+newParagraph()
+
+// Function expression
+
+const newList = function() {
+  console.log("inside newList")
+  const firstPara = document.querySelector("p")
+  const newList = document.createElement("ul")
+  newList.innerHTML =
+    `<li> The most important thing</li>
+    <li> The last thing</li>`
+  firstPara.prepend(newList)
+}
+newList()
+
+// Arrow function
+ let bob = () => {
+  console.log("inside arrow")
+  const firstUl = document.querySelector("ul")
+  const newHeading = document.createElement("h2")
+  newHeading.innerText = "Here is your list"
+  firstUl.prepend(newHeading)
+}
+bob()

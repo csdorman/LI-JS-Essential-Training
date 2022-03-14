1f1c073e1644ec8d5a869177191ee1b1ea6c486b
csdorman <csdorman@gmail.com>
2022-03-14 12:03:04 -0400

    Working on addEventListeners

--- Practice/09_04/script.js
+++ Practice/09_04/script.js
@@ -5,3 +5,20 @@
  * - Add an event listener to each grid cell to change its background color when it is clicked.
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
+
+const gridCell = document.querySelectorAll(".cell")
+const grid = document.querySelector(".grid")
+
+
+window.addEventListener("click", (e) => {
+  console.log("Click event registered", e)
+})
+
+grid.addEventListener("mouseenter", (e) => {
+  grid.setAttribute("class", "grid-mouseOver")
+})
+
+grid.addEventListener("mouseleave", (e) => {
+  grid.setAttribute("class", "grid")
+})
+
